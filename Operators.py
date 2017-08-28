f0bd4fa9d96962dbb0c4c37cebedc73100f7e34c
svrswetha <svrswetha@gmail.com>
2017-08-28 15:19:26 -0500

    Calculator is ready with Exceptions Handled with while loops

--- Operators.py
+++ Operators.py
@@ -19,30 +19,39 @@ def div(number1, number2):
   return number1 / number2
 
 def main():
-  validInput = False
-  while not validInput:
-    try:
-      num1 = int(input("What is number 1\n"))
-      num2 = int(input("What is number 2\n"))
-      operation = int(input("what do you want to do? 1)Add 2)Subtract 3)Multiply 4)Division"))
-      validInput = True
-    except:
-      print("Invalid Input..Try Again")
+  start = True
+  while start:
+    validInput = False
+    while not validInput:
+      # Get User Input
+      try:
+        num1 = int(input("What is number 1\n"))
+        num2 = int(input("What is number 2\n"))
+        operation = int(input("what do you want to do? 1)Add 2)Subtract 3)Multiply 4)Division "))
+        validInput = True
+      except:
+        print("Invalid Input.. Try Again")
 
 
-  if (operation == 1):
-    print("Adding....")
-    print(add(num1, num2))
-  elif (operation == 2):
-    print("Subtracting....")
-    print(sub(num1, num2))
-  elif (operation == 3):
-    print("Multiplying....")
-    print(mul(num1, num2))
-  elif (operation == 4):
-    print("Division....")
-    print(div(num1, num2))
-  else:
-    print("Idontknow")
-
+    if (operation == 1):
+      print("Adding....")
+      print(add(num1, num2))
+    elif (operation == 2):
+      print("Subtracting....")
+      print(sub(num1, num2))
+    elif (operation == 3):
+      print("Multiplying....")
+      print(mul(num1, num2))
+    elif (operation == 4):
+      print("Division....")
+      print(div(num1, num2))
+    else:
+      print("Idontknow")
+    trigger = raw_input("Do you wanna continue, if yes enter 'y' else enter anything other than 'y'")
+    if(trigger == 'y'):
+      main()
+    else:
+      print("Exiting....!")
+      return
+    break
 main()
