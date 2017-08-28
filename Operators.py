e1480a189cf75bcb89165bd3971f6a36637040d1
svrswetha <svrswetha@gmail.com>
2017-08-28 00:56:55 -0500

    Calculator is ready with Exceptions Handled

--- Operators.py
+++ Operators.py
@@ -1,38 +1,59 @@
 """Calculator"""
 
+""" Perform basic calculations with 2 numbers"""
+
 """Addition"""
 
-def add(number1,number2):
-    return number1 + number2
+
+def add(number1, number2):
+  return number1 + number2
+
 
 """Subtraction"""
+
+
 def sub(number1, number2):
-    return number1 - number2
+  return number1 - number2
 
 
 """Multiplication"""
+
+
 def mul(number1, number2):
-    return number1 * number2
+  return number1 * number2
 
 
 """Division"""
-def div(number1,number2):
-    return number1/number2
+
+
+def div(number1, number2):
+  return number1 / number2
 
 def main():
-    num1 = int(input("Enter Number 1\n"))
-    num2 = int(input("Enter Number 2\n"))
-    operation =raw_input("what do you want to do? 1)Add 2)Subtract 3)Multiply 4)Division")
+  validInput = False
+  while not validInput:
+    try:
+      num1 = int(input("What is number 1\n"))
+      num2 = int(input("What is number 2\n"))
+      operation = int(input("what do you want to do? 1)Add 2)Subtract 3)Multiply 4)Division"))
+      validInput = True
+    except:
+      print("Invalid Input..Try Again")
 
-    if(operation ==  '1'):
-        print(add(num1,num2))
-    elif(operation == '2'):
-        print(sub(num1,num2))
-    elif(operation == '3'):
-        print(mul(num1,num2))
-    elif(operation == '4'):
-        print(div(num1,num2))
-    else:
-        print("Idontknow")
+
+  if (operation == 1):
+    print("Adding....")
+    print(add(num1, num2))
+  elif (operation == 2):
+    print("Subtracting....")
+    print(sub(num1, num2))
+  elif (operation == 3):
+    print("Multiplying....")
+    print(mul(num1, num2))
+  elif (operation == 4):
+    print("Division....")
+    print(div(num1, num2))
+  else:
+    print("Idontknow")
 
 main()
