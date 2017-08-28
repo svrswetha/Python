e5b589d0d96d3127f06d594f432aa38ccbab3107
svrswetha <svrswetha@gmail.com>
2017-08-28 00:20:11 -0500

    Calculator is ready!

--- Operators.py
+++ Operators.py
@@ -1,32 +1,38 @@
-
-
-"""Addition"""
+"""Calculator"""
 
 """Addition"""
+
 def add(number1,number2):
     return number1 + number2
 
 """Subtraction"""
-def subtract(number1, number2):
+def sub(number1, number2):
     return number1 - number2
 
 
 """Multiplication"""
-def multiply(number1, number2):
+def mul(number1, number2):
     return number1 * number2
 
 
 """Division"""
-def division(number1,number2):
+def div(number1,number2):
     return number1/number2
 
 def main():
-    num1 = int(input("Enter Number 1"))
-    num2 = int(input("Enter Number 2"))
+    num1 = int(input("Enter Number 1\n"))
+    num2 = int(input("Enter Number 2\n"))
+    operation =raw_input("what do you want to do? 1)Add 2)Subtract 3)Multiply 4)Division")
 
-    print add(num1,num2)
-    print subtract(num1,num2)
-    print multiply(num1,num2)
-    print division(num1,num2)
+    if(operation ==  '1'):
+        print(add(num1,num2))
+    elif(operation == '2'):
+        print(sub(num1,num2))
+    elif(operation == '3'):
+        print(mul(num1,num2))
+    elif(operation == '4'):
+        print(div(num1,num2))
+    else:
+        print("Idontknow")
 
 main()
